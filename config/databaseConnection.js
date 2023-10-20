718681457cce2ca13d87dec1abfba53acaed2df1
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-20 01:56:40 -0700

    setting up folders and empty files

---
+++ config/databaseConnection.js
@@ -0,0 +1,22 @@
+// import sequelize library, which is an ORM library for node designed to work with relational databases
+// when you have imported sequelize, you can use it to define models, interact with databases, perform CRUD operations, etc
+const Sequelize = require("sequelize");
+
+// loads all env variables into the process and places it in the process.env
+require("dotenv").config();
+
+// creating a Sequelize instance and define a database connection
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // database name
+  process.env.DB_USER, // user
+  process.env.DB_PASSWORD, // password
+
+  // database location
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
