98da992f6fda6f0b7db5317cf271a8000317da72
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-21 01:23:30 -0700

    Comment.js created

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+// id user_id, post_id, content
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/databaseConnection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Comment",
+  }
+);
+
+module.exports = Comment;
