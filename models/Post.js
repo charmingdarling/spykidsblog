cfa73350c1b076c8122c572671bc1f6e073a1c17
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-21 01:25:07 -0700

    Post Model created

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/databaseConnection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Post",
+  }
+);
+
+module.exports = Post;
