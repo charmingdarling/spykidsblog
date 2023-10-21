aec2528089ab659d70fb528ad56c428489a1b420
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-21 01:23:58 -0700

    relationships established

--- models/index.js
+++ models/index.js
@@ -1,37 +1,39 @@
 const User = require("./User");
-// const Post = require("./Post");
-// const Comment = require(".Comment");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
-// // In Post.js
-// Post.hasMany(Comment, {
-//   foreignKey: "post_id",
-//   onDelete: "CASCADE",
-// });
+// In Post.js
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
 
-// Post.belongsTo(User, {
-//   foreignKey: "user_id",
-//   onDelete: "CASCADE",
-// });
+// Associate Post to User
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
-// // In Comment.js
+// In Comment.js
 
-// Comment.belongsTo(Post, {
-//   foreignKey: "post_id",
-// });
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
 
-// Comment.belongsTo(User, {
-//   foreignKey: "user_id",
-//   onDelete: "CASCADE",
-// });
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
-// // In User.js
+// In User.js
 
-// User.hasMany(Post, {
-//   foreignKey: "user_id",
-// });
+// Associate User with Post
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
 
-// User.hasMany(Comment, {
-//   foreignKey: "user_id",
-// });
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
 
-module.exports = { User };
+module.exports = { User, Post };
