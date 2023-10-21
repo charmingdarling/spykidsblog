e7da2e738be7f4c8aab331d70d09c8d41e723900
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-20 22:57:11 -0700

    got rid of homeRoutes, not appropriate in the api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,7 @@
 const router = require("express").Router();
 
-// const homeRoutes = require("./homeRoutes");
-
 const userRoutes = require("./userRoutes");
 
 router.use("/user", userRoutes);
-
-// router.use("/", homeRoutes);
 
 module.exports = router;
