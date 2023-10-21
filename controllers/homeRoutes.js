e1f4cb3e0cf6ca6d4a9ff495d438eb2328e901c7
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-20 22:55:27 -0700

    router.get login created

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,11 +4,20 @@ const { User } = require("../models");
 // Note: If you are in your homeRoutes, you may need to change this to /register to get register.handlebars
 
 router.get("/", async (req, res) => {
-  console.log("Line 6 GET");
+  console.log("Line 7 router.GET of homeRoutes");
   try {
-    res.render("register");
+    res.render("homepage");
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
   }
 });
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    return res.redirect("/profile");
+  }
+  res.render("login");
+});
+
+module.exports = router;
