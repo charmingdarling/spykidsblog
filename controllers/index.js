534124719e97393119a1c0a05cbe1c60bc8ece8e
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-23 00:34:45 -0700

    routes added and used

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
-const homeRoutes = require("./homeRoutes");
 const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+const profileRoutes = require("./profileRoutes");
 
-router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/", profileRoutes);
 
 module.exports = router;
