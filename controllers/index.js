3d60721e8a7b89e6e5e316e147913b98d4c3ad67
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-24 01:20:30 -0700

    reordered the layout

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,22 @@
+// Router called to listen to different endpoints
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 const profileRoutes = require("./profileRoutes");
 
-router.use("/api", apiRoutes);
-router.use("/", homeRoutes);
-router.use("/", profileRoutes);
+// Front-end request
+// The requests in between the " _ " incoming from the user.
+// http://localhost:3001/
+// http://localhost:3001/api
+// http://localhost:3001/profile
 
+// Then if we are getting a request for one of those endpoints, THEN, use the routes.
+// - then use homeRoutes, apiRoutes, or profileRoutes to handle the requests
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/profile", profileRoutes);
+
+// exporting the router so it can be in the index
 module.exports = router;
