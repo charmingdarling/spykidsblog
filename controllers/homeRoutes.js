38494b502c6f86483bfa3d8bdaf86df984553422
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-24 01:21:07 -0700

    router to get a user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,42 +2,43 @@ const router = require("express").Router();
 const { User, Post } = require("../models");
 const withAuth = require("../utils/auth");
 
-// // Note: If you are in your homeRoutes, you may need to change this to /register to get register.handlebars
+// Note: If you are in your homeRoutes, you may need to change this to /register to get register.handlebars
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const postData = await Post.findAll({
-//       include: [
-//         {
-//           model: User,
-//         },
-//       ],
-//     });
-//     const posts = postData.map((post) => post.get({ plain: true }));
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-//     req.session.save(() => {
-//        // set up a session variable to count the number of times we visit the homepage
-//       if (req.session.countVisit) {
-//       // if the `countVisit` session variables already exists, increment it by 1
-//       req.session.countVisit++;
-//       } else {
-//       // if the `countVisit` session doesn't exist, set it to 1
-//       } req.session.countVisit = 1;
+    req.session.save(() => {
+      // set up a session variable to count the number of times we visit the homepage
+      if (req.session.countVisit) {
+        // if the `countVisit` session variables already exists, increment it by 1
+        req.session.countVisit++;
+      } else {
+        // if the `countVisit` session doesn't exist, set it to 1
+      }
+      req.session.countVisit = 1;
 
-//       res.render('/profile', {
-//         posts,
-//         // send over the current `countVisit` session variable to be rendered
-//         countVisit: req.session.countVisit,
-//       });
-//     });
-//   } catch(err) {
-//     console.error(err);
-//     res.status(500).json(err);
-//   }
-// });
+      res.render("homepage", {
+        posts,
+        logged_in: req.session.logged_in,
+      });
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
 //     // Pass serialized data and session flag into template
 //     // When using `res.render`, you provide the view name without a leading slash'/'
+//     // res.renders the "homepage"
 //     res.render("homepage", {
 //       posts,
 //       logged_in: req.session.logged_in,
@@ -57,4 +58,5 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// exporting the router so it can be in the index
 module.exports = router;
