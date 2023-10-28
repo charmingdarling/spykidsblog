5d5257f16ab13ad27b9009d7276639606e110704
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-27 20:57:18 -0700

    added profileRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
+const profileRoutes = require("./profileRoutes");
 const userRoutes = require("./userRoutes");
 const postRoutes = require("./postRoutes");
 
+router.use("/profile", profileRoutes);
 router.use("/user", userRoutes);
 router.use("/post", postRoutes);
 
