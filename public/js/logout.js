0578df6c2f6ae9573c6233e11bfc4564aef65b74
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-27 20:52:27 -0700

    /api/user/logout -- corrected from 'users'

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,8 +1,9 @@
 const logout = async () => {
-  const response = await fetch("/api/users/logout", {
+  const response = await fetch("/api/user/logout", {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
   });
+  console.log(response);
 
   if (response.ok) {
     document.location.replace("/login");
