7d8d62d0e021cef9e16ec362f2312918efbef1b0
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-28 22:25:54 -0700

    signup formhandler removed

--- public/js/login.js
+++ public/js/login.js
@@ -39,34 +39,6 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (username && email && password) {
-    const response = await fetch("/api/user", {
-      method: "POST",
-      // stringify data, because data in transmission can only be sent as a string
-      // except when it gets to our routes, it has already been parsed due to server.js having "app.use(express.json());" Example of Parse: Username = "John Smith"
-      body: JSON.stringify({ username, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
-
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
