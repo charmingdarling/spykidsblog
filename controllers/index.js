1cbb7ed237ae0403e292c2f1c2dfaa26f9128301
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-28 17:48:27 -0700

    notes adjusted

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,12 @@
-// Router called to listen to different endpoints
-const router = require("express").Router();
+const router = require("express").Router(); // Router called to listen to different endpoints
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 const profileRoutes = require("./profileRoutes");
 
-// Front-end request
-// The requests in between the " _ " incoming from the user.
-// http://localhost:3001/
-// http://localhost:3001/api
-// http://localhost:3001/profile
+// Front-end request " " incoming from user; after the comma is what we call the request handler
+router.use("/", homeRoutes); // http://localhost:3001/
+router.use("/api", apiRoutes); // http://localhost:3001/api
+router.use("/profile", profileRoutes); // http://localhost:3001/profile
 
-// Then if we are getting a request for one of those endpoints, THEN, use the routes.
-// - then use homeRoutes, apiRoutes, or profileRoutes to handle the requests
-
-router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
-router.use("/profile", profileRoutes);
-
-// exporting the router so it can be in the index
-module.exports = router;
+module.exports = router; // exporting the router so it can be in the index
