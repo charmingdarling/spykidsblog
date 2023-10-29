623f6e19d45a56e01530767c8c4cfd7bc4d223ff
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-28 22:25:14 -0700

    signup formhandler moved here

---
+++ public/js/signup.js
@@ -0,0 +1,30 @@
+// This code is used in the client-side JavaScript to handle form submissions for user login.
+// It assumes that the server is set up to handle login requests at the "api/user/signup" endpoint
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/user", {
+      method: "POST",
+      // stringify data, because data in transmission can only be sent as a string
+      // except when it gets to our routes, it has already been parsed due to server.js having "app.use(express.json());" Example of Parse: Username = "John Smith"
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
