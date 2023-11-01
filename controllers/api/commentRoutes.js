f067facf174b12b2f17e4e2936eac8eaa6dd49c2
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-01 16:02:02 -0700

    getting comments by post id and creating new comment

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,56 @@
+const { User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+const router = require("express").Router();
+
+// Route to create a new comment (POST Request), route is private so it uses withAuth middleware
+// http://localhost:3001/api/comment/
+// in the public/js/comment.js file, the fetch request is sent to this route
+router.post("/:", withAuth, async (req, res) => {
+  try {
+    // Get data from the request body
+    console.log(req.body); // Checking to see if the comment data is being sent by client
+    const commentData = await Comment.create({
+      ...req.body,
+      // These are key-value pairs inside the req.body object
+      user_id: req.session.user_id,
+      // on the front end there is a token that is sent with the request to the backend
+      username: req.session.username,
+    });
+    res.json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Route to get all comments (GET Request)
+// http://localhost:3001/api/comment/:id
+router.get("/:id", withAuth, async (req, res) => {
+  try {
+    // Get all comments and JOIN with user data
+    const commentData = await Comment.findAll({
+      // This is the Comment model
+      where: { post_id: req.params.id }, // Post id
+      include: [
+        {
+          model: User, // User model
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    // If no comments found, return an error
+    if (!commentData || commentData.length < 1) {
+      return res
+        .status(404)
+        .json({ message: "No comments found with this id" });
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
