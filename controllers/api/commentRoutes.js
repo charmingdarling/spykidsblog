0f2985214f280cd8cf05a64bd310898c67e505c3
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-03 23:44:49 -0700

    router added, variable created for req.body; content, post_id: req.body.postId added to the comment Data variable object; router.get - const postID = req.params.id added so I could use it within the comment Data variable object.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,21 +1,24 @@
+const router = require("express").Router();
 const { User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
-const router = require("express").Router();
 
 // Route to create a new comment (POST Request), route is private so it uses withAuth middleware
 // http://localhost:3001/api/comment/
 // in the public/js/comment.js file, the fetch request is sent to this route
-router.post("/:", withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     // Get data from the request body
     console.log(req.body); // Checking to see if the comment data is being sent by client
+    const { content } = req.body;
     const commentData = await Comment.create({
-      ...req.body,
+      content,
+      post_id: req.body.postId,
       // These are key-value pairs inside the req.body object
       user_id: req.session.user_id,
       // on the front end there is a token that is sent with the request to the backend
       username: req.session.username,
     });
+
     res.json(commentData);
   } catch (err) {
     console.log(err);
@@ -27,10 +30,11 @@ router.post("/:", withAuth, async (req, res) => {
 // http://localhost:3001/api/comment/:id
 router.get("/:id", withAuth, async (req, res) => {
   try {
+    const postID = req.params.id;
     // Get all comments and JOIN with user data
     const commentData = await Comment.findAll({
       // This is the Comment model
-      where: { post_id: req.params.id }, // Post id
+      where: { post_id: postID }, // Post id
       include: [
         {
           model: User, // User model
@@ -46,6 +50,7 @@ router.get("/:id", withAuth, async (req, res) => {
         .json({ message: "No comments found with this id" });
     }
 
+    // res.status(200).json({ ...commentData.toJSON() });
     res.status(200).json(commentData);
   } catch (err) {
     console.log(err);
