72702c8fd33ce67217b734ccea94e71f777a5971
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-03 23:46:13 -0700

    removed everything - this route is unnecessary

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,28 +1,3 @@
 const router = require("express").Router();
-const { User, Post, Comment } = require("../../models");
-const withAuth = require("../../utils/auth");
-
-//! please fix the render part - no renderings or send for api routes
-
-router.get("/newPost", withAuth, (req, res) => {
-  res.render("new-post", {
-    layout: "profile",
-  });
-});
-
-//! please fix the render part - no renderings or send for api routes
-
-router.get("/edit/:id", withAuth, async (req, res) => {
-  try {
-    const editData = await Post.findByPk(req.params.id);
-    if (editData) {
-      const edit = editData.get({ plain: true });
-      res.render("edit", edit);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(400).json(err);
-  }
-});
 
 module.exports = router;
