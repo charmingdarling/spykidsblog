5283908c1233e753ba5a2527e3782b2b59125fec
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-06 16:28:48 -0800

    fixing content of the update post

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,16 @@
-//This is the one for updates WORKING
+// This logic goes to the postRoutes in your /controllers/api
+// This is the one for updates WORKING
 document
   .querySelector("#update-form")
   .addEventListener("submit", async (event) => {
-    event.preventDefault();
-    console.log("update-form submitted");
-    // Get the post ID, title, content fom the ID element in HTML on edit.handlebars
-    const postID = document.querySelector("#postID").value;
-    const title = document.querySelector("#title").value.trim();
-    const content = document.querySelector("#update-content").value.trim();
     try {
+      event.preventDefault();
+      console.log("update-form submitted");
+      // Get the post ID, title, content from the ID element in HTML on edit.handlebars
+      const postID = document.querySelector("#postID").value; // ID of the post we want to edit
+      const title = document.querySelector("#title").value.trim();
+      const content = document.querySelector("#update-content").value.trim();
+
       if (title && content) {
         const response = await fetch(`/api/post/${postID}`, {
           method: "PUT",
@@ -26,6 +28,7 @@ document
         alert("Please fill out both title and content.");
       }
     } catch (error) {
-      alert(error);
+      console.error(error);
+      alert("An error occurred while updating the post.");
     }
   });
